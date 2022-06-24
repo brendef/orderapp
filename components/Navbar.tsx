6b1567ff9ba6938bf629a0ceb5cac529e8eb20d5
Brendan de Faria <brendan.defaria@gmail.com>
2022-06-24 23:11:33 +0200

    added Maraki Ui Navbar, still needs state for menu
    Still needs to be made dynamic

---
+++ components/Navbar.tsx
@@ -0,0 +1,64 @@
+// React.js
+import React from 'react'
+
+const Navbar = () => {
+
+  // Scopped CSS
+  const renderCSS = () => {
+    <style jsx> {`
+      .scroll-hidden::-webkit-scrollbar {
+        height: 0px;
+        background: transparent;
+      }
+    `} </style>
+  }
+
+  return (
+    <>
+      {renderCSS()}
+
+      <nav className="bg-white shadow dark:bg-gray-800">
+        <div className="container px-6 py-3 mx-auto">
+          <div className="flex flex-col md:flex-row md:justify-between md:items-center">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <a className="text-2xl font-bold text-gray-800 transition-colors duration-200 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" href="#">Brand</a>
+              </div>
+              {/* Mobile menu button */}
+              <div className="flex md:hidden">
+                <button type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+                  <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
+                    <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            {/* Mobile Menu open: "block", Menu closed: "hidden"  */}
+            
+            {/* 
+            
+            - Create state to conditionally add hidden or block to show and hide menu, invert the state by clicking on the button
+            - Change the icon of the button based on state too 
+            */}
+
+            <div className="items-center md:flex">
+              <div className="flex flex-col mt-2 md:flex-row md:mt-0 md:mx-1">
+                <a className="my-1 text-sm leading-5 text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:mx-4 md:my-0" href="#">Home</a>
+                <a className="my-1 text-sm leading-5 text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:mx-4 md:my-0" href="#">Blog</a>
+                <a className="my-1 text-sm leading-5 text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:mx-4 md:my-0" href="#">Compoents</a>
+                <a className="my-1 text-sm leading-5 text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:mx-4 md:my-0" href="#">Courses</a>
+              </div>
+              <div className="flex items-center py-2 -mx-1 md:mx-0">
+                <a className="block w-1/2 px-3 py-2 mx-1 text-sm font-medium leading-5 text-center text-white transition-colors duration-200 transform bg-gray-500 rounded-md hover:bg-blue-600 md:mx-2 md:w-auto" href="#">Login</a>
+                <a className="block w-1/2 px-3 py-2 mx-1 text-sm font-medium leading-5 text-center text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-600 md:mx-0 md:w-auto" href="#">Join free</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+    </>
+  )
+}
+
+export default Navbar
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,14 @@
 import type { NextPage } from 'next'
 // Components
 import Layout from '../components/Layout'
+import Navbar from '../components/Navbar'
 // -----------------------------------------------------------------------------------------------------------------------
 
 // index
 const Home: NextPage = () => {
   return (
     <Layout>
-      hello world!
+      <Navbar />
     </Layout>
   )
 }
