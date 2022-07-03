b0b1404dc7be7ed64a008f7396badbf9c6b255ce
Brendan de Faria <brendan.defaria@gmail.com>
2022-07-03 17:01:34 +0200

    set up forgot password

--- firebase/contexts/AuthContext.tsx
+++ firebase/contexts/AuthContext.tsx
@@ -1,24 +1,24 @@
 // React JS
 import { createContext, useContext, useEffect, useState } from 'react'
 // Firebase
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { createUserWithEmailAndPassword, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 // Auth 
 import { auth } from '../config'
 
 const AuthContext = createContext<any>({})
 
-export const AuthContextProvider = ({ children }:any) => {
+export const AuthContextProvider = ({ children }: any) => {
 
   const [user, setUser] = useState<any>(null)
   const [loading, setLoading] = useState(true)
 
-  const createUser = (email:any, password:any) => {
+  const createUser = (email: any, password: any) => {
     // Place code to register a user with a provider here
     // this case: firebase
     return createUserWithEmailAndPassword(auth, email, password)
   }
 
-  const authenticateUser = (email:any, password:any) => {
+  const authenticateUser = (email: any, password: any) => {
     // Place code to register a user with a provider here
     // this case: firebase
     return signInWithEmailAndPassword(auth, email, password)
@@ -29,17 +29,21 @@ export const AuthContextProvider = ({ children }:any) => {
     await signOut(auth)
   }
 
+  const forgotPassword = (email:any) => {
+    return sendPasswordResetEmail(auth, email)
+  }
+
   useEffect(() => {
-    
-    const unsubscribe = onAuthStateChanged(auth, (currentUser:any) => {
+
+    const unsubscribe = onAuthStateChanged(auth, (currentUser: any) => {
       currentUser ? setUser(currentUser) : setUser(null)
       setLoading(false)
     })
 
     return () => unsubscribe()
   }, [])
-  
-  return <AuthContext.Provider value={{ user, createUser, authenticateUser }}>{ loading ? null : children }</AuthContext.Provider>
+
+  return <AuthContext.Provider value={{ user, createUser, authenticateUser, forgotPassword }}>{loading ? null : children}</AuthContext.Provider>
 }
 
 export const useAuth = () => useContext(AuthContext)
