16d49f178dcf71ed0cdfc0e63343b1284dc799be
Brendan de Faria <brendan.defaria@gmail.com>
2022-07-07 00:19:10 +0200

    made card more dynamic

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,45 +1,36 @@
 import React from 'react'
 
-const Card = () => {
-  return (
-    
-    <div className="flex max-w-md mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
-        <div className="w-1/3 bg-cover"></div>
+const Card = ({
+    title = 'Card header',
+    body = 'Lorem ipsum dolor sit amet consectetur adipisicing elit In odit',
+    price = '300',
+    currency = 'R'
+}) => {
 
-        <div className="w-2/3 p-4 md:p-4">
-            <h1 className="text-2xl font-bold text-gray-800 dark:text-white">Backpack</h1>
+    return (
 
-            <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit In odit</p>
+        <div className="flex max-w-md mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
+            <div className="w-1/3 bg-blue-500">  </div>
 
-            <div className="flex mt-2 item-center">
-                <svg className="w-5 h-5 text-gray-700 fill-current dark:text-gray-300" viewBox="0 0 24 24">
-                    <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
-                </svg>
+            <div className="w-2/3 p-4 md:p-4">
+                <h1 className="text-2xl font-bold text-gray-800 dark:text-white">{ title }</h1>
 
-                <svg className="w-5 h-5 text-gray-700 fill-current dark:text-gray-300" viewBox="0 0 24 24">
-                    <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
-                </svg>
+                <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">{ body }</p>
 
-                <svg className="w-5 h-5 text-gray-700 fill-current dark:text-gray-300" viewBox="0 0 24 24">
-                    <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
-                </svg>
+                <div className="flex justify-between mt-5 item-center">
 
-                <svg className="w-5 h-5 text-gray-500 fill-current" viewBox="0 0 24 24">
-                    <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
-                </svg>
+                    <h1 className="text-3xl mt-2 font-bold text-gray-700 dark:text-gray-200 md:text-2xl">
+                        {currency} { price }
+                    </h1>
+                    <button className="px-5 py-4 text-xs font-bold text-white uppercase transition-colors duration-200 transform bg-gray-800 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600">
+                        Add to Cart
+                    </button>
 
-                <svg className="w-5 h-5 text-gray-500 fill-current" viewBox="0 0 24 24">
-                    <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
-                </svg>
-            </div>
-
-            <div className="flex justify-between mt-5 item-center">
-                <h1 className="text-3xl mt-1 font-bold text-gray-700 dark:text-gray-200 md:text-2xl">R220</h1>
-                <button className="px-4 py-3 text-xs font-bold text-white uppercase transition-colors duration-200 transform bg-gray-800 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600">Add to Cart</button>
+                </div>
             </div>
         </div>
-    </div>
-  )
+
+    )
 }
 
 export default Card
